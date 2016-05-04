49a82e99f6a25bb2c5bc9683619751631b1cd5bf
Casper Co <haln22111@gmail.com>
2016-05-04 14:26:24 -0700

    nullValueCheckProgress

--- app/businessRules/nullValueCheck.rb
+++ app/businessRules/nullValueCheck.rb
@@ -1,1 +1,11 @@
-print 'something'
+require 'csv'
+def nullValueCheck(table, column)
+  filepath = "data/in/tables".table.".csv"
+  writer = CSV.open("data/out/tables".table."failures.csv", "wb")
+  CSV.foreach(filepath, :headers => true) do |row|
+    if row[column] == "" || row[column] == "null" || row[column] == nil
+      #row << "Failed the null value check business rule."
+      writer << row
+    end
+  end
+end
