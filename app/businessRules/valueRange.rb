8e307394b8ed9361c2da1156d6ab5fedcf00c492
Justin Walk <justinwalk@live.com>
2016-05-04 10:10:36 -0700

    deleted duplicate

--- app/businessRules/valueRange.rb
+++ app/businessRules/valueRange.rb
@@ -1,5 +1,7 @@
 def valuesInRange(csvfile, params)
-CSV.foreach("#{Rails.root}/db/data.csv", {:headers => true, :header_converters => :symbol}) do |row|
-end
-return failedLines
+    CSV.foreach(csvfile, {:headers => true, :header_converters => :symbol}) do |row|
+        if (params[:column] >= params[:minValue] && params[:column] <= params[:maxValue])
+        end
+    end
+    return failedLines
 end
--- app/valueRange.rb
+++
@@ -1,7 +0,0 @@
-def valuesInRange(csvfile, params)
-    CSV.foreach(csvfile, {:headers => true, :header_converters => :symbol}) do |row|
-        if (params[:column] >= params[:minValue] && params[:column] <= params[:maxValue])
-        end
-    end
-    return failedLines
-end
