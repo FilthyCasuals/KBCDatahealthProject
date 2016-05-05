8068f6b501739eeee0c8559139f6bf8b501a9e45
jimmi <jimmilin0531@gmail.com>
2016-05-05 04:11:25 -0700

    data type check finishing comment

--- app/businessRules/dataTypeCheck.rb
+++ app/businessRules/dataTypeCheck.rb
@@ -7,15 +7,15 @@ module DataTypeCheck
       csv.each do |row|
 
           case params[:dataType]
-
+          # check for alphanumeric
           when 'alphanumeric'
-
             if(row[:"#{params[:column]}"].match(/^[[:alnum:]]+$/) rescue false)
                 Common::buildCSV(row.values, "pass")
             else
                 row[:failure_reason]  = "#{params[:column]} is not alphanumeric"
                 Common::buildCSV(row.values, "fail")
             end
+          #check for decimal
           when 'decimal'
             stuff = row[:"#{params[:column]}"]
             if(stuff.match(/^\d+.\d+$/))
@@ -24,6 +24,7 @@ module DataTypeCheck
                 row[:failure_reason]  = " #{params[:column]} is not decimal"
                 Common::buildCSV(row.values, "fail")
             end
+          #check for integer
           when 'integer'
             if(Integer(row[:"#{params[:column]}"]) rescue false)
                 Common::buildCSV(row.values, "pass")
@@ -33,6 +34,5 @@ module DataTypeCheck
             end
           end
       end
-
   end
 end
