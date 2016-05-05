e2af6c1bd55f26f6c498ab2477f74a71362938a3
jimmi <jimmilin0531@gmail.com>
2016-05-04 21:32:54 -0700

    dataTypeCheck v3

--- app/businessRules/dataTypeCheck.rb
+++ app/businessRules/dataTypeCheck.rb
@@ -5,8 +5,30 @@ def DataTypeCheck(csvFile, columnName, dataType)
     # @column = columnName
     # @type = dataType
 
-    CSV.foreach(csv, headers:true) do |row|
-
+    csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
+    csv.each do |row|
+        put case columnName
+        when 'id'
+          if(row[:"#{columnName}"].match(/^[[:alnum:]]+$/))
+              Common::buildCSV(row.values, "pass")
+          else
+              row[:failure_reason]  = "#{columnName} is not alphanumeric"
+              common::buildCSV(row.values, "fail")
+          end
+        when 'Amount'
+          if(row[:"#{columnName}"].to_f.match(/[0-9]\.]+/))
+              Common::buildCSV(row.values, "pass")
+          else
+              row[:failure_reason]  = " #{columnName} is not decimal"
+              common::buildCSV(row.values, "fail")
+          end
+        when 'Probability'
+          if(Integer(row[:"#{columnName}"]) rescue false)
+              Common::buildCSV(row.values, "pass")
+          else
+              row[:failure_reason]  = " #{columnName} is not decimal"
+              common::buildCSV(row.values, "fail")
+          end
     end
 
 end
