86a2fd153f82a8aae300862f264eab9921229765
jimmi <jimmilin0531@gmail.com>
2016-05-05 02:03:41 -0700

    dataTypeCheck v4

--- app/businessRules/dataTypeCheck.rb
+++ app/businessRules/dataTypeCheck.rb
@@ -1,34 +1,38 @@
-require 'csv'
+module DataTypeCheck
 
-def DataTypeCheck(csvFile, columnName, dataType)
-    # @csv = csvfile
-    # @column = columnName
-    # @type = dataType
 
-    csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
-    csv.each do |row|
-        put case columnName
-        when 'id'
-          if(row[:"#{columnName}"].match(/^[[:alnum:]]+$/))
-              Common::buildCSV(row.values, "pass")
-          else
-              row[:failure_reason]  = "#{columnName} is not alphanumeric"
-              common::buildCSV(row.values, "fail")
-          end
-        when 'Amount'
-          if(row[:"#{columnName}"].to_f.match(/[0-9]\.]+/))
-              Common::buildCSV(row.values, "pass")
-          else
-              row[:failure_reason]  = " #{columnName} is not decimal"
-              common::buildCSV(row.values, "fail")
-          end
-        when 'Probability'
-          if(Integer(row[:"#{columnName}"]) rescue false)
-              Common::buildCSV(row.values, "pass")
-          else
-              row[:failure_reason]  = " #{columnName} is not decimal"
-              common::buildCSV(row.values, "fail")
-          end
-    end
+  def applyDataTypeCheck(csvFile, params)
+      csv = CSV.open(csvFile, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
 
+      csv.each do |row|
+
+          case params[:dataType]
+
+          when 'alphanumeric'
+
+            if(row[:"#{params[:column]}"].match(/^[[:alnum:]]+$/) rescue false)
+                Common::buildCSV(row.values, "pass")
+            else
+                row[:failure_reason]  = "#{params[:column]} is not alphanumeric"
+                Common::buildCSV(row.values, "fail")
+            end
+          when 'decimal'
+            stuff = row[:"#{params[:column]}"]
+            if(stuff.match(/^\d+.\d+$/))
+                Common::buildCSV(row.values, "pass")
+            else
+                row[:failure_reason]  = " #{params[:column]} is not decimal"
+                Common::buildCSV(row.values, "fail")
+            end
+          when 'integer'
+            if(Integer(row[:"#{params[:column]}"]) rescue false)
+                Common::buildCSV(row.values, "pass")
+            else
+                row[:failure_reason]  = " #{params[:column]} is not Integer"
+                Common::buildCSV(row.values, "fail")
+            end
+          end
+      end
+
+  end
 end
