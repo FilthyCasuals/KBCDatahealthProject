179e601a36e3e222c6c689b135139953f3a69c10
Justin Walk <justinwalk@live.com>
2016-05-05 10:21:51 -0700

    add module

--- app/businessRules/valueRange.rb
+++ app/businessRules/valueRange.rb
@@ -1,9 +1,11 @@
-def valuesInRange(csvinput, csvoutput, params)
+module valueRange
 
-    CSV.foreach(csvinput, {:headers => true, :header_converters => :symbol}) do |row|
-        unless (row[params[:column]] >= params[:minValue] && row[params[:column]] <= params[:maxValue])
-            row << [failure_reason: "Not in value range"]
-            csvoutpout << row.values
+    def valuesInRange(csvinput, csvoutput, params)
+        CSV.foreach(csvinput, {:headers => true, :header_converters => :symbol}) do |row|
+            unless (row[params[:column]] >= params[:minValue] && row[params[:column]] <= params[:maxValue])
+                row << [failure_reason: "Not in value range"]
+                csvoutpout << row.values
+            end
         end
     end
 
