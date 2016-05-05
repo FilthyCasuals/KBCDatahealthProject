f265873cab35748adc5e427086bbd04bc2745102
Justin Walk <justinwalk@live.com>
2016-05-05 11:51:03 -0700

    set up for test

--- app/businessRules/valueRange.rb
+++ app/businessRules/valueRange.rb
@@ -1,12 +1,14 @@
 module valueRange
 
-    def valuesInRange(csvinput, csvoutput, params)
-        CSV.foreach(csvinput, {:headers => true, :header_converters => :symbol}) do |row|
+    def applyRule(csvin, params)
+        csvout = []
+        CSV.foreach(csvin, {:headers => true, :header_converters => :symbol}) do |row|
             unless (row[params[:column]] >= params[:minValue] && row[params[:column]] <= params[:maxValue])
                 row << [failure_reason: "Not in value range"]
-                csvoutpout << row.values
+                csvout << row.values
             end
         end
+        return csvout
     end
 
 end
--- main.rb
+++ main.rb
@@ -1,9 +1,24 @@
 Dir["/app/businessRules/*.rb"].each {|file| require file }
 require "csv"
 
-headers = CSV.open(csvfile, 'r') { |csv| csv.first }
-headers << "failure_reason"
 
-CSV.open("/data/out/tables/valueRange.csv", "wb") do |csv|
-    csv << headers
+#variables
+csvsource = "/data/in/tables/opportunity.csv"
+csvdestination = "/data/out/tables/destination.csv"
+
+#Set up headers for csvoutput file, based on columns from input
+headers = CSV.open(csvsource, 'r') { |csv| csv.first }
+headers << "failure_reason"
+CSV.open(csvdestination, "wb") do |csvout|
+    csvout << headers
+end
+
+#apply business rules to input data
+params = [column: amount, minValue: 0.00, maxValue: 10000.00]
+failedRows = []
+failedRows << valueRange.applyRule(csvsource, params)
+CSV.open(csvdestination, "a+") do |csvout|
+    failedRows.each do |row|
+        csvout << row
+    end
 end
