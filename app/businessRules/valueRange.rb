930d14dc474f70a88b65845e4dc2dc5245c36e26
Justin Walk <justinwalk@live.com>
2016-05-06 11:32:06 -0700

    some fixes to logic

--- app/businessRules/valueRange.rb
+++ app/businessRules/valueRange.rb
@@ -2,10 +2,20 @@ module ValueRange
 
     def applyRule(csvin, params)
         csvout = []
-                row << [failure_reason: "Not in value range"]
+        # CSV.foreach(csvin, {:headers => true, :header_converters => :symbol}) do |row|
+        #     unless (row[params[:column]] >= params[:minValue] && row[params[:column]] <= params[:maxValue])
+        #         row << [failure_reason: "Not in value range"]
+        #         csvout << row.values
+        #     end
+        # end
+        csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
+        csv.each do |row|
+            unless (row[:"#{params[:column]}"].to_f >= params[:minValue] && row[:"#{params[:column]}"].to_f <= params[:maxValue])
+                row[:failure_reason]  = "Not in value range"
                 csvout << row.values
             end
         end
+        print csvout
         return csvout
     end
 
