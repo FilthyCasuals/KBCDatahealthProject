cb7bd27151b97282724b5d7fc10742b97843c36a
Casper Co <haln22111@gmail.com>
2016-05-06 11:23:49 -0700

    Maybe finished nullValueCheck rule
    By maybe Casper

--- app/businessRules/nullValueCheck.rb
+++ app/businessRules/nullValueCheck.rb
@@ -1,11 +1,28 @@
 require 'csv'
-def nullValueCheck(csvinput, csvoutput, column)
-  filepath = "data/in/tables"+table+".csv"
-  writer = CSV.open("data/out/tables"+table+"failures.csv", "wb")
-  CSV.foreach(filepath, :headers => true) do |row|
-    if row[column] == "" || row[column] == "null" || row[column] == nil
-      #row << "Failed the null value check business rule."
-      writer << row
+require 'fileutils'
+require 'tempfile'
+
+def nullValueCheck(csvinput, csvoutputpass, csvoutputfail, column)
+  filepath = "/data/in/tables/"+csvinput
+  tableOutPath = "/data/out/tables/"
+  passed = TempFile.new(csvoutputpass)
+  failed = TempFile.new(csvoutputfail)
+  #passed = CSV.open("/data/out/tables/"+csvoutputpass, "wb")
+  #failures = CSV.open("/data/out/tables/"+csvoutputfail, "wb")
+  failureField = "Failed the null value check"
+  CSV.open(passed, w) do |passedCSV|
+    CSV.open(failed, w) do |failedCSV|
+      CSV.foreach(filepath, :headers => true) do |row|
+        if row[column] == "" || row[column] == "null" || row[column] == nil
+          #row << "Failed the null value check business rule."
+          row << [failureField]
+          failed << row
+        else
+          passed << row
+        end
+      end
     end
   end
+  FileUtils.mv(passed, tableOutPath+csvoutputpass, :force => true)
+  FileUtils.mv(failed, tableOutPath+csvoutputfail, :force => true)
 end
