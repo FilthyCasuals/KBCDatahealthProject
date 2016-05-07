1d09cb6ca8b5128cebd98b23b640b7fae1292b34
Casper Co <haln22111@gmail.com>
2016-05-07 16:31:07 -0700

    Quick mod to null value

--- app/businessRules/nullValueCheck.rb
+++ app/businessRules/nullValueCheck.rb
@@ -2,27 +2,14 @@ require 'csv'
 require 'fileutils'
 require 'tempfile'
 
-def nullValueCheck(csvinput, csvoutputpass, csvoutputfail, params)
-  filepath = "/data/in/tables/"+csvinput
-  tableOutPath = "/data/out/tables/"
-  passed = TempFile.new(csvoutputpass)
-  failed = TempFile.new(csvoutputfail)
-  #passed = CSV.open("/data/out/tables/"+csvoutputpass, "wb")
-  #failures = CSV.open("/data/out/tables/"+csvoutputfail, "wb")
-  failureField = "Failed the null value check"
-  CSV.open(passed, w) do |passedCSV|
-    CSV.open(failed, w) do |failedCSV|
-      CSV.foreach(csvinput, :headers => true) do |row|
-        if row[params[:column]] == "" || row[params[:column]] == "null" || row[params[:column]] == nil
-          #row << "Failed the null value check business rule."
-          row << [failureField]
-          failed << row
-        else
-          passed << row
-        end
-      end
+def applyRule(csvinput, params)
+  failureField = "Failed the null or empty field check"
+  CSV.foreach(csvinput, :headers => true) do |row|
+    if (row[params[:column]] == "" || row[params[:column]] == "null" || row[params[:column]] == nil)
+      row << failureField
+      Common::buildCSV(row, "fail")
+    else
+      Common::buildCSV(row, "pass")
     end
   end
-  FileUtils.mv(passed, tableOutPath+csvoutputpass, :force => true)
-  FileUtils.mv(failed, tableOutPath+csvoutputfail, :force => true)
 end
