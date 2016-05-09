55177f48e699cd5105c23d59bd8448d4a6cae5f9
Casper Co <haln22111@gmail.com>
2016-05-09 09:54:45 -0700

    Null value check module fix

--- app/businessRules/nullValueCheck.rb
+++ app/businessRules/nullValueCheck.rb
@@ -1,15 +1,16 @@
 require 'csv'
 require 'fileutils'
 require 'tempfile'
-
-def applyRule(csvinput, params)
-  failureField = "Failed the null or empty field check"
-  CSV.foreach(csvinput, :headers => true) do |row|
-    if (row[params[:column]] == "" || row[params[:column]] == "null" || row[params[:column]] == nil)
-      row << failureField
-      Common::buildCSV(row, "fail")
-    else
-      Common::buildCSV(row, "pass")
+module NullValueCheck
+  def applyRule(csvinput, params)
+    failureField = "Failed the null or empty field check"
+    CSV.foreach(csvinput, :headers => true) do |row|
+      if (row[params[:column]] == "" || row[params[:column]] == "null" || row[params[:column]] == nil)
+        row << failureField
+        Common::buildCSV(row, "fail")
+      else
+        Common::buildCSV(row, "pass")
+      end
     end
   end
 end
