dce0c172af2eac8101cca0b13ddff50f0ee68b56
Justin Walk <justinwalk@live.com>
2016-05-09 11:05:14 -0700

    Known values rule

---
+++ app/businessRules/knownValues.rb
@@ -0,0 +1,15 @@
+module KnownValues
+
+    def applyRule(csvin, params)
+        csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
+        csv.each do |row|
+            if (params[:values].include? row[:"#{params[:column]}"])
+                Common::buildCSV(row.values, "pass")
+            else
+                row[:failure_reason]  = "#{params[:column]} not a known value"
+                Common::buildCSV(row.values, "fail")
+            end
+        end
+    end
+
+end
