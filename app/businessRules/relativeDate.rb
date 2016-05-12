048e94f0b30198eaddd3ed54ccc059dd83385e6f
evelyndai <daiyi68@live.cn>
2016-05-12 13:52:38 -0700

    Added error checking for picked date input

--- app/businessRules/relativeDate.rb
+++ app/businessRules/relativeDate.rb
@@ -1,32 +1,27 @@
 module RelativeDate
 require 'date'
   def applyRelativeDate(csvin, params)
-    if(Date.parse(row[:"#{params[:pickeDate]}"]) rescue false)
-      case params[:timeFormat]
-      when "YYYY-MM-DD"
-        timeFormat = "%Y-%m-%d"
-        return checkMatch(csvin, timeFormat, params)
-      when "YYYY-MM"
-        timeFormat = "%Y-%m"
-        return checkMatch(csvin, timeFormat, params)
-      when "YYYY"
-        timeFormat = "%Y"
-        return checkMatch(csvin, timeFormat, params)
-      when "DD/MM/YYYY"
-        timeFormat = "%d/%m/%Y"
-        return checkMatch(csvin, timeFormat, params)
-      when "MM/DD/YYYY"
-        timeFormat = "%m/%d/%Y"
-        return checkMatch(csvin, timeFormat, params)
-      when "YYYYMMDD"
-        timeFormat = "%Y%m%d"
-        return checkMatch(csvin, timeFormat, params)
-      else
-      end
 
+    case params[:timeFormat]
+    when "YYYY-MM-DD"
+      timeFormat = "%Y-%m-%d"
+      return checkMatch(csvin, timeFormat, params)
+    when "YYYY-MM"
+      timeFormat = "%Y-%m"
+      return checkMatch(csvin, timeFormat, params)
+    when "YYYY"
+      timeFormat = "%Y"
+      return checkMatch(csvin, timeFormat, params)
+    when "DD/MM/YYYY"
+      timeFormat = "%d/%m/%Y"
+      return checkMatch(csvin, timeFormat, params)
+    when "MM/DD/YYYY"
+      timeFormat = "%m/%d/%Y"
+      return checkMatch(csvin, timeFormat, params)
+    when "YYYYMMDD"
+      timeFormat = "%Y%m%d"
+      return checkMatch(csvin, timeFormat, params)
     else
-      #if the picked date is not a valid input (date format) throw an error msg
-
     end
   end
 
@@ -36,29 +31,31 @@ require 'date'
     column = params[:column]
     pickDate = params[:datePicked]
     direction = params[:direction]
-
-    #iterate the input file
-    csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
-    csv.each do |row|
-      #convert the input date from custom format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
-      begin
-        dateTime = DateTime.strptime(row[:"#{column}"], timeFormat)
-      rescue ArgumentError
-        row[:failure_reason]  = "Doesn't match the given date format."
-        Common::buildCSV(row.values, "fail")
-      else
-        #convert the picked date from any format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
-        pickDateTime = DateTime.strptime(pickDate, timeFormat)
-
-
-        #check if it's in the past/future relative to picked date
-        if dateTime <= pickDateTime && direction == "past"
-          Common::buildCSV(row.values, "pass")
-        elsif dateTime >= pickDateTime && direction == "future"
-          Common::buildCSV(row.values, "pass")
-        else
-          row[:failure_reason]  = "This #{column} is not #{direction} of #{pickDate}."
+    begin
+      #convert the picked date from any format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
+      pickDateTime = DateTime.strptime(pickDate, timeFormat)
+    rescue ArgumentError
+      return false
+    else
+      #iterate the input file
+      csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
+      csv.each do |row|
+        #convert the input date from custom format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
+        begin
+          dateTime = DateTime.strptime(row[:"#{column}"], timeFormat)
+        rescue ArgumentError
+          row[:failure_reason]  = "Doesn't match the given date format."
           Common::buildCSV(row.values, "fail")
+        else
+          #check if it's in the past/future relative to picked date
+          if dateTime <= pickDateTime && direction == "past"
+            Common::buildCSV(row.values, "pass")
+          elsif dateTime >= pickDateTime && direction == "future"
+            Common::buildCSV(row.values, "pass")
+          else
+            row[:failure_reason]  = "This #{column} is not #{direction} of #{pickDate}."
+            Common::buildCSV(row.values, "fail")
+          end
         end
       end
     end
