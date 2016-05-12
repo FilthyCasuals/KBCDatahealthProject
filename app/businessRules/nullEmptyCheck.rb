0f208b727c432a75bc6a3a6bfbf93cd4e7211535
Casper Co <haln22111@gmail.com>
2016-05-12 09:28:39 -0700

    Null Empty Check Schema Addition
    Along with minor character case change

--- app/businessRules/nullEmptyCheck.rb
+++ app/businessRules/nullEmptyCheck.rb
@@ -6,7 +6,7 @@ module NullEmptyCheck
     failureField = "Failed the null or empty field check"
     CSV.foreach(csvinput, :headers => true, :header_converters => :symbol, :converters => :all) do |row|
       row << row.to_hash
-      if (row[:"#{params[:column]}"] == "" || row[:"#{params[:column]}"] == "null" || row[:"#{params[:column]}"] == nil)
+      if (row[:"#{params[:column]}"] == "" || row[:"#{params[:column]}"] == nil)
         row << failureField
         Common::buildCSV(row, "fail")
       else
