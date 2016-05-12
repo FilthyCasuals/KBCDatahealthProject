5eb93cbe646198c1f7489feb9bef8fb7b88a82c9
Justin Walk <justinwalk@live.com>
2016-05-12 11:18:12 -0700

    setting up main to use passed in params

--- main.rb
+++ main.rb
@@ -1,25 +1,24 @@
+require 'rubygems'
 Dir["./app/businessRules/*.rb"].each {|file| require file }
-require "csv"
 require "./common/lib.rb"
+require "csv"
+require 'json'
 
 include Common
 include ValueRange
 include KnownValues
 include RelativeDate
+include DataTypeCheck
+include DateFormatCheck
+include NullEmptyCheck
+include StringLength
+
 #variables
 csvSource = "./test/data/in/tables/opportunity.csv"
+paramSource = "./test/data/in/files/sample.json"
 
 #Set up headers for csvoutput file, based on columns from input
 Common::buildHeaders(csvSource)
-
-#Test data for passed in business rules structure
-requestedRules =
-[
-    [rule: "applyValueRange", params: { column: "amount", minValue: 0.00, maxValue: 180000.00 }],
-    [rule: "applyKnownValues", params: { column: "stagename", values: ["Closed Lost", "Closed Won"] }],
-    #[rule: "applyRelativeDate", params: {column: "createddate", direction:"future", datePicked:"2011-05-01", timeFormat:"YYYY-MM-DD"}]
-
-]
 
 #apply business rules to input data
 requestedRules.each do |ruleData|
