f0b42dfe9334b437e3891bc7058fa801459acac2
evelyndai <daiyi68@live.cn>
2016-05-13 12:14:31 -0700

    added error handling for relative date

--- app/businessRules/relativeDate.rb
+++ app/businessRules/relativeDate.rb
@@ -32,32 +32,38 @@ require 'date'
     pickDate = params[:datePicked]
     direction = params[:direction]
     begin
-      #convert the picked date from any format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
-      pickDateTime = DateTime.strptime(pickDate, timeFormat)
+      checkRelative(csvin,timeFormat,direction,pickDate,column)
     rescue ArgumentError
       return false
-    else
-      #iterate the input file
-      csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
-      csv.each do |row|
-        #convert the input date from custom format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
-        begin
-          dateTime = DateTime.strptime(row[:"#{column}"], timeFormat)
-        rescue ArgumentError
-          row[:failure_reason]  = "Doesn't match the given date format."
-          Common::buildCSV(row.values, "fail")
+
+    end
+  end
+
+  def checkRelative(csvin,timeFormat,direction,pickDate,column)
+    #convert the picked date from any format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
+    pickDateTime = DateTime.strptime(pickDate, timeFormat)
+    #iterate the input file
+    csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
+    csv.each do |row|
+      #convert the input date from custom format to standard format as <DateTime: 2001-02-03T04:05:06+07:00 ...>
+      begin
+        dateTime = DateTime.strptime(row[:"#{column}"], timeFormat)
+      rescue ArgumentError
+        row[:failure_reason]  = "Doesn't match the given date format."
+        Common::buildCSV(row.values, "fail")
+      else
+        #check if it's in the past/future relative to picked date
+        if dateTime <= pickDateTime && direction == "past"
+          Common::buildCSV(row.values, "pass")
+        elsif dateTime >= pickDateTime && direction == "future"
+          Common::buildCSV(row.values, "pass")
         else
-          #check if it's in the past/future relative to picked date
-          if dateTime <= pickDateTime && direction == "past"
-            Common::buildCSV(row.values, "pass")
-          elsif dateTime >= pickDateTime && direction == "future"
-            Common::buildCSV(row.values, "pass")
-          else
-            row[:failure_reason]  = "This #{column} is not #{direction} of #{pickDate}."
-            Common::buildCSV(row.values, "fail")
-          end
+          row[:failure_reason]  = "This #{column} is not #{direction} of #{pickDate}."
+          Common::buildCSV(row.values, "fail")
         end
       end
     end
   end
+
+
 end
--- app/businessRules/stringLength.rb
+++ app/businessRules/stringLength.rb
@@ -7,10 +7,9 @@ module StringLength
     failureField = params[:column] + " failed the string length check"
     CSV.foreach(csvinput, :headers => true, :header_converters => :symbol, :converters => :all) do |row|
       row << row.to_hash
-      if(row[:"#{params[:column]}"] == nil){
+      if(row[:"#{params[:column]}"] == nil)
         row << failureField
         Common::buildCSV(row, "fail")
-      }
       elsif (params[:rules][:strLen])
         if (row[:"#{params[:column]}"].size == params[:rules][:strLen])
           Common::buildCSV(row, "pass")
