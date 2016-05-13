9e6949b98928bd7dd3912681f2d7d771efd15ad8
Justin Walk <justinwalk@live.com>
2016-05-13 10:34:35 -0700

    fixed known values

--- app/businessRules/knownValues.rb
+++ app/businessRules/knownValues.rb
@@ -1,9 +1,17 @@
 module KnownValues
 
     def knownValues(csvin, params)
+
+        #build array of know values
+        values = []
+        params[:values].each do |name|
+            values << name[:name]
+        end
+
+        #parse column to see if value is known
         csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
         csv.each do |row|
-            if (params[:values].include? row[:"#{params[:column]}"])
+            if (values.include? row[:"#{params[:column]}"])
                 Common::buildCSV(row.values, "pass")
             else
                 row[:failure_reason]  = "#{params[:column]} not a known value"
