aab4fbd37853642a08e7b3803e6e5de594595483
Justin Walk <justinwalk@live.com>
2016-05-16 09:53:27 -0700

    add check for string values in min/max

--- app/businessRules/valueRange.rb
+++ app/businessRules/valueRange.rb
@@ -1,13 +1,16 @@
 module ValueRange
 
     def valueRange(csvin, params)
-        csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
-        csv.each do |row|
-            if (row[:"#{params[:column]}"].to_f >= params[:minValue] && row[:"#{params[:column]}"].to_f <= params[:maxValue])
-                Common::buildCSV(row.values, "pass")
-            else
-                row[:failure_reason]  = "#{params[:column]} not in value range: #{params[:minValue]} - #{params[:maxValue]}"
-                Common::buildCSV(row.values, "fail")
+        #If passed values that are not floats, rule is skipped. Needs some form of feedback in future development
+        if (params[:minValue].is_a?(Float) && params[:maxValue].is_a?(Float))
+            csv = CSV.open(csvin, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
+            csv.each do |row|
+                if (row[:"#{params[:column]}"].to_f >= params[:minValue] && row[:"#{params[:column]}"].to_f <= params[:maxValue])
+                    Common::buildCSV(row.values, "pass")
+                else
+                    row[:failure_reason]  = "#{params[:column]} not in value range: #{params[:minValue]} - #{params[:maxValue]}"
+                    Common::buildCSV(row.values, "fail")
+                end
             end
         end
     end
