b88b0d8c4a984b4d009fc095ecc122da079f7d18
Justin Walk <justinwalk@live.com>
2016-05-17 11:32:56 -0700

    revert

--- app/businessRules/stringLength.rb
+++ app/businessRules/stringLength.rb
@@ -4,16 +4,16 @@ require 'tempfile'
 module StringLength
   def stringLength(csvinput, params)
     failureField = params[:column] + " failed the string length check"
-    csv = CSV.open(csvinput, :headers => true, :header_converters => :symbol).to_a.map {|row| row.to_hash}
-    csv.each do |row|
+    CSV.foreach(csvinput, :headers => true, :header_converters => :symbol, :converters => :all) do |row|
+      row = row.to_hash
       if(row[:"#{params[:column]}"] == nil)
-        row[:failure_reason] = failureField
+        row << failureField
         Common::buildCSV(row, "fail")
       elsif (params[:options][:strLen])
         if (row[:"#{params[:column]}"].size == params[:options][:strLen])
           Common::buildCSV(row, "pass")
         else
-          row[:failure_reason] = failureField
+          row << failureField
           Common::buildCSV(row, "fail")
         end
       elsif (params[:options][:minLen] != 0 || params[:options][:maxLen] != 0)
@@ -21,19 +21,19 @@ module StringLength
           if (row[:"#{params[:column]}"].size >= params[:options][:minLen])
             Common::buildCSV(row, "pass")
           else
-            row[:failure_reason] = failureField
+            row << failureField
             Common::buildCSV(row, "fail")
           end
         else
           if (row[:"#{params[:column]}"].size >= params[:options][:minLen] && row[:"#{params[:column]}"].size <= params[:options][:maxLen])
             Common::buildCSV(row, "pass")
           else
-            row[:failure_reason] = failureField
+            row << failureField
             Common::buildCSV(row, "fail")
           end
         end
       else
-        row[:failure_reason] = "Error occured, invalid string length setting."
+        row << "Error occured, invalid string length setting."
         Common::buildCSV(row, "fail")
       end
     end
