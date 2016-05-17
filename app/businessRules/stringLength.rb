bf296bb1f618da0d0886d336f97bc11383089b62
Jimmi Lin <A00889404@ad.bcit.ca>
2016-05-17 11:49:14 -0700

    strlen fix v1.48
    options = params[:options].to_hash

--- app/businessRules/stringLength.rb
+++ app/businessRules/stringLength.rb
@@ -4,28 +4,29 @@ require 'tempfile'
 module StringLength
   def stringLength(csvinput, params)
     failureField = params[:column] + " failed the string length check"
+    options = params[:options].to_hash
     CSV.foreach(csvinput, :headers => true, :header_converters => :symbol, :converters => :all) do |row|
       row = row.to_hash
       if(row[:"#{params[:column]}"] == nil)
         row << failureField
         Common::buildCSV(row, "fail")
-      elsif (params[:options][:strLen])
-        if (row[:"#{params[:column]}"].size == params[:options][:strLen])
+      elsif (options[:strLen])
+        if (row[:"#{params[:column]}"].size == options[:strLen])
           Common::buildCSV(row, "pass")
         else
           row << failureField
           Common::buildCSV(row, "fail")
         end
-      elsif (params[:options][:minLen] != 0 || params[:options][:maxLen] != 0)
-        if (params[:options][:maxLen] == 0)
-          if (row[:"#{params[:column]}"].size >= params[:options][:minLen])
+      elsif (options[:minLen] != 0 || options[:maxLen] != 0)
+        if (options[:maxLen] == 0)
+          if (row[:"#{params[:column]}"].size >= options[:minLen])
             Common::buildCSV(row, "pass")
           else
             row << failureField
             Common::buildCSV(row, "fail")
           end
         else
-          if (row[:"#{params[:column]}"].size >= params[:options][:minLen] && row[:"#{params[:column]}"].size <= params[:options][:maxLen])
+          if (row[:"#{params[:column]}"].size >= options[:minLen] && row[:"#{params[:column]}"].size <= options[:maxLen])
             Common::buildCSV(row, "pass")
           else
             row << failureField
