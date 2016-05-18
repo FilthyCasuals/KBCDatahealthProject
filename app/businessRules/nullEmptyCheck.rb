c842dcb52832a18d601e1c9149d24cfccc82d86b
Casper Co <haln22111@gmail.com>
2016-05-18 11:35:52 -0700

    Casper changes 10

--- app/businessRules/nullEmptyCheck.rb
+++ app/businessRules/nullEmptyCheck.rb
@@ -4,14 +4,18 @@ require 'tempfile'
 module NullEmptyCheck
   def nullEmptyCheck(csvinput, params)
     failureField = "Failed the null or empty field check"
-    row = Array.new
-    CSV.foreach(csvinput, {:encoding => "UTF-8", :headers => true, :header_converters => :symbol, :converters => :all}) do |csvRow|
-      row << csvRow.to_hash
-      if (row[:"#{params[:column]}"] == "" || row[:"#{params[:column]}"] == nil)
-        row << failureField
-        Common::buildCSV(row, "fail")
-      else
-        Common::buildCSV(row, "pass")
+    #row = Array.new
+    csv = csv.open(csvinput, {:encoding => "UTF-8", :headers => true, :header_converters => :symbol, :converters => :all}).to_a.map {|row| row.to_hash}
+    csv.each do |row|
+    #CSV.foreach(csvinput, {:encoding => "UTF-8", :headers => true, :header_converters => :symbol, :converters => :all}) do |csvRow|
+      #row << csvRow.to_hash
+      if(row != nil)
+        if (row[:"#{params[:column]}"] == "" || row[:"#{params[:column]}"] == nil)
+          row << failureField
+          Common::buildCSV(row, "fail")
+        else
+          Common::buildCSV(row, "pass")
+        end
       end
     end
   end
